76e333642f01ba9a1a4fc6a122cad47767571641
7H4N0S <tanushbamnote6@gmail.com>
2025-07-07 21:28:59 +0530

    Removed a line by Tanush.

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -27,11 +27,7 @@ const HomeContent = () => (
                 <FigmaContactForm />
             </div>
             <SectionSpacer size="medium" />
-            <div className='w-full flex flex-row gap-20 justify-center items-center pb-20'>
-                <Teamcard />
-                <Teamcard />
-                <Teamcard />
-            </div>
+            
             <Footer />
         </div>
     </div>
